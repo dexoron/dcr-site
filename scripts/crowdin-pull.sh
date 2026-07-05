37b1435f71bad23b98eb5eb97764795d56a2bafa
Dexoron <probrovova1@gmail.com>
2026-07-05 18:34:00 +0800

    update crowdin api adress

--- scripts/crowdin-pull.sh
+++ scripts/crowdin-pull.sh
@@ -4,4 +4,7 @@ if [ -z "${CROWDIN_TOKEN:-}" ]; then
   echo "CROWDIN_TOKEN not set, skipping translation pull"
   exit 0
 fi
-bunx @crowdin/cli pull --token "$CROWDIN_TOKEN" --project-id "$CROWDIN_PROJECT_ID"
+bunx @crowdin/cli pull \
+  --token "$CROWDIN_TOKEN" \
+  --project-id "$CROWDIN_PROJECT_ID" \
+  --base-url "https://dcr.api.crowdin.com"
